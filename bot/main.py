53aaedb202c5171363e450381ccbeb7a29ce5cd3
iogi0 <93683278+iogi0@users.noreply.github.com>
2023-04-22 22:32:49 +0300

    Add files via upload

--- bot/main.py
+++ bot/main.py
@@ -2,6 +2,8 @@ import logging
 import aiogram
 import random
 from aiogram import Bot,Dispatcher,types,executor
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
 
 TOKEN = '6133107533:AAHElHmcdx7jFK2wmNg4amsLpF6Ev0rr6zY'
 
@@ -14,12 +16,12 @@ number = random.randint(1, 100)
 
 @dp.message_handler(commands='start')
 async def start(message: types.Message):
-    await message.answer('Привіт! я пропоную тобі вгадати число,яке я загадав (від 1 до 100).що б розпочати гру нажми на /game') #, reply_markup=start_button
+    await message.answer('Привіт! я пропоную тобі вгадати число,яке я загадав (від 1 до 100).що б розпочати гру нажми на кнопку' , reply_markup=start_button) #, reply_markup=start_button
 
-# @dp.callback_query_handler()
-# async def get_info(callback_query: types.CallbackQuery):
-#     if callback_query.data == 'start':
-#         await message.ansver('123')
+@dp.callback_query_handler()
+async def get_info(callback_query: types.CallbackQuery):
+    if callback_query.data == 'start':
+        await bot.send_message(callback_query.from_user.id,'гру розпочато! напиши число від 1 до 100')
 
 @dp.message_handler(commands='game')
 async def game(message: types.Message):
@@ -37,17 +39,15 @@ async def guess_number(message: types.Message):
             await message.reply('я загадав менше число')
 # якщо користувач вгадує число,гра закінчується
         else:
-            await message.reply('ви вгадали! що б почати нову гру введіть команду /game ')
+            await message.reply('ви вгадали! що б почати нову гру нажміть на кнопку', reply_markup=start_button) 
     except:
         await message.reply('я сказав число від 1 до 100.')
 
-# from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-
-# start_button = InlineKeyboardMarkup(
-#     inline_keyboard=[
-#         InlineKeyboardButton(text='почати гру',callback_data='game')
-#     ]
-# )
+start_button = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [InlineKeyboardButton(text='почати гру',callback_data='start')]
+    ]
+)
     
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
